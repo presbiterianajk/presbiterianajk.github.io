c75630da3a59778720bea67e1dc0af83287f37ad
julioeler <julioeler221@gmail.com>
2023-07-04 09:20:09 -0300

    add js nav mobile

--- script.js
+++ script.js
@@ -33,6 +33,61 @@
   var closeBtn = document.getElementById("close-btn");
   closeBtn.addEventListener("click", closePopup);
 
+
+
+
+
+// FUNÇÃO MOBILE
+document.addEventListener("DOMContentLoaded", function() {
+  // Inicialmente, exiba a seção "Escala" e adicione a classe "active" ao link correspondente
+  document.querySelector("#escala").classList.add("active");
+  document.querySelector("a[href='#escala']").classList.add("active");
+
+  // Adicione um evento de clique a todos os links da navegação
+  var links = document.querySelectorAll("nav ul li a");
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function(event) {
+      event.preventDefault();
+
+      // Remova a classe "active" de todas as seções e links
+      var sections = document.querySelectorAll("section");
+      for (var j = 0; j < sections.length; j++) {
+        sections[j].classList.remove("active");
+      }
+      for (var k = 0; k < links.length; k++) {
+        links[k].classList.remove("active");
+      }
+
+      // Adicione a classe "active" à seção correspondente e ao link clicado
+      var target = this.getAttribute("href").substring(1);
+      document.querySelector("#" + target).classList.add("active");
+      this.classList.add("active");
+    });
+  }
+});
+
+// Verifica se o dispositivo é móvel antes de aplicar o código JavaScript
+if (/Mobi|Android/i.test(navigator.userAgent)) {
+  document.addEventListener("DOMContentLoaded", function() {
+    // Código JavaScript para alternar as seções
+    // ...
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // function showPopup() {
   //   var overlay = document.getElementById("overlay");
   //   var popup = document.getElementById("popup");
