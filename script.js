189368217ac770a5397950b47ff0844641717ac6
julioeler <julioeler221@gmail.com>
2023-07-16 12:26:14 -0300

    desativado pop up aviso

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 // POP UP QUANDO ENTRAR PELA PRIMEIRA VEZ NO SITE NO DIA
 
-function showPopup() {
-    var overlay = document.getElementById("overlay");
-    var popup = document.getElementById("popup");
-    overlay.classList.add("show-popup");
-    popup.classList.add("show-popup");
-  }
+// function showPopup() {
+//     var overlay = document.getElementById("overlay");
+//     var popup = document.getElementById("popup");
+//     overlay.classList.add("show-popup");
+//     popup.classList.add("show-popup");
+//   }
 
-  function closePopup() {
-    var overlay = document.getElementById("overlay");
-    var popup = document.getElementById("popup");
-    overlay.classList.remove("show-popup");
-    popup.classList.remove("show-popup");
-  }
+//   function closePopup() {
+//     var overlay = document.getElementById("overlay");
+//     var popup = document.getElementById("popup");
+//     overlay.classList.remove("show-popup");
+//     popup.classList.remove("show-popup");
+//   }
 
-  function isNewDay() {
-    var lastVisitDate = localStorage.getItem("lastVisitDate");
-    var currentDate = new Date().toLocaleDateString();
+//   function isNewDay() {
+//     var lastVisitDate = localStorage.getItem("lastVisitDate");
+//     var currentDate = new Date().toLocaleDateString();
 
-    if (lastVisitDate === currentDate) {
-      return false;
-    } else {
-      localStorage.setItem("lastVisitDate", currentDate);
-      return true;
-    }
-  }
+//     if (lastVisitDate === currentDate) {
+//       return false;
+//     } else {
+//       localStorage.setItem("lastVisitDate", currentDate);
+//       return true;
+//     }
+//   }
 
-  // Verifica se é um novo dia e exibe o pop-up se for a primeira visita do dia
-  if (isNewDay()) {
-    showPopup();
-  }
+//   // Verifica se é um novo dia e exibe o pop-up se for a primeira visita do dia
+//   if (isNewDay()) {
+//     showPopup();
+//   }
 
-  // Fecha o pop-up quando o botão 'Fechar' é clicado
-  var closeBtn = document.getElementById("close-btn");
-  closeBtn.addEventListener("click", closePopup);
+//   // Fecha o pop-up quando o botão 'Fechar' é clicado
+//   var closeBtn = document.getElementById("close-btn");
+//   closeBtn.addEventListener("click", closePopup);
 
 
 // FUNÇÃO MOBILE NAV
