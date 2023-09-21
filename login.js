584ae35581e7b2c600d7acd1598bf73de4703f63
julioeler <julioeler221@gmail.com>
2023-09-21 16:11:30 -0300

    correção js login

--- login.js
+++ login.js
@@ -1,11 +1,14 @@
 var auth = firebase.auth();
 
-function login() {
+function login(event) {
+  event.preventDefault(); // Evita o comportamento padrão do botão (recarregar a página)
+  
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
 
   auth.signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
+      alert('Login bem-sucedido!');
       // Login bem-sucedido, redirecione para a página de administração
       window.location.href = 'admin.html';
     })
@@ -15,3 +18,8 @@ function login() {
     });
 }
 
+// Adicione um ouvinte de evento para o formulário no arquivo login.js
+document.addEventListener('DOMContentLoaded', function () {
+  const loginForm = document.getElementById('loginForm');
+  loginForm.addEventListener('submit', login);
+});
